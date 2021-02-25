5db81944aeefee3801fe2789eb9fd5e1425a8343
Joao Nascimento <jhonyzito4gb@gmail.com>
2021-02-25 02:19:43 -0300

    sql do exercício 1.2 e 1.3 feitos, resta logica e sql do exercicio 1.4

---
+++ 1.3/SQLQuery2 PetShop.sql
@@ -0,0 +1,31 @@
+CREATE DATABASE petshop;
+
+USE petshop;
+CREATE TABLE Veterinarian
+(
+	idVeterinarian INT PRIMARY KEY IDENTITY,
+	VeterinarianName VARCHAR(100) NOT NULL,
+);
+CREATE TABLE Type
+(
+	idType INT PRIMARY KEY IDENTITY,
+	Spicie VARCHAR(100) NOT NULL,
+);
+CREATE TABLE Owner
+(
+	idOwner INT PRIMARY KEY IDENTITY,
+	OwnerName VARCHAR(100) NOT NULL,
+);
+CREATE TABLE Pet
+(
+	idPet INT PRIMARY KEY IDENTITY,
+	idType INT FOREIGN KEY REFERENCES Type(idType),
+	idOwner INT FOREIGN KEY REFERENCES Owner(idOwner),
+);
+CREATE TABLE Clinic
+(
+	idClinic INT PRIMARY KEY IDENTITY,
+	idVeterinarian INT FOREIGN KEY REFERENCES Veterinarian(idVeterinarian),
+	idPet  INT FOREIGN KEY REFERENCES Pet(idPet),
+	ClinicName VARCHAR(100) NOT NULL,
+);
