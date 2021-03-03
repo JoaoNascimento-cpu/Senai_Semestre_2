d5f2082b6c6e82976128fcecfc16b321d245c0eb
Joao Nascimento <jhonyzito4gb@gmail.com>
2021-03-03 17:20:09 -0300

    RPG 1.0 Feito DDL, DML e DQL

---
+++ RPG/Hroads_01_DDL.sql
@@ -0,0 +1,35 @@
+USE Senai_Hroads_Tarde;
+
+CREATE DATABASE Senai_Hroads_Tarde;
+
+CREATE TABLE Classes
+(
+	idClasses INT PRIMARY KEY IDENTITY,
+	NomeClasse VARCHAR(50) NOT NULL,
+);
+CREATE TABLE TiposHabilidades
+(
+	idTiposHabilidades INT PRIMARY KEY IDENTITY,
+	Tipo VARCHAR(200) NOT NULL,
+); 
+CREATE TABLE Habilidades
+(
+	idHabilidade INT PRIMARY KEY IDENTITY,
+	NomeHabilidade VARCHAR(30) NOT NULL,
+	idTiposHabilidades INT FOREIGN KEY REFERENCES TiposHabilidades(idTiposHabilidades),
+);
+CREATE TABLE ClassesHabilidades
+(
+	idClasses INT FOREIGN KEY REFERENCES Classes(idClasses),
+	idHabilidades INT FOREIGN KEY REFERENCES Habilidades(idHabilidade)
+);
+CREATE TABLE Personagens
+(
+	idPersonagem INT PRIMARY KEY IDENTITY,
+	Nome VARCHAR(255) NOT NULL,
+	idClasse INT FOREIGN KEY REFERENCES Classes(idClasses),
+	PvMaximo INT,
+	ManaMaxima INT,
+	DataAtt VARCHAR(20) NOT NULL,
+	DataCr VARCHAR(20) NOT NULL,
+);
